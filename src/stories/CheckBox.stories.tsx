68a46c7a64fa0bca23dfda169e45a52fb6db98b8
syoo970 <syu9710@gmail.com>
2022-08-01 14:10:50 +0900

    [feat] checkbox 완성

--- src/stories/CheckBox.stories.tsx
+++ src/stories/CheckBox.stories.tsx
@@ -6,6 +6,35 @@ export default {
   component: CheckBox,
 } as ComponentMeta<typeof CheckBox>;
 
+const Template: ComponentStory<typeof CheckBox> = (args) => (
+  <CheckBox {...args} />
+);
+
 export const IconCheckBox: ComponentStory<typeof CheckBox> = () => (
   <CheckBox useIcon iconName="favorite_border" checkedIconName="favorite" />
 );
+
+export const WithLabel: ComponentStory<typeof CheckBox> = () => (
+  <CheckBox label="With label" />
+);
+
+export const DefaultCheck: ComponentStory<typeof CheckBox> = () => (
+  <CheckBox defaultCheck />
+);
+
+export const Size: ComponentStory<typeof CheckBox> = () => (
+  <div>
+    <CheckBox size="small" />
+    <CheckBox size="medium" />
+  </div>
+);
+
+export const CustomSize = Template.bind({});
+CustomSize.args = {
+  fontSize: 20,
+};
+
+export const CustomColor = Template.bind({});
+CustomColor.args = {
+  customColor: "red",
+};
