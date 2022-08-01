c03b1cf4274ce21f515771872cc26234d3791422
syoo970 <syu9710@gmail.com>
2022-08-01 19:37:40 +0900

    [fix] multi select 예제 수정

--- src/stories/Select.stories.tsx
+++ src/stories/Select.stories.tsx
@@ -1,6 +1,5 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
-import { useForm } from "react-hook-form";
-import { SelectChangeEvent } from "@mui/material/Select";
+import { Controller, useForm } from "react-hook-form";
 import Select from "@/components/Select";
 
 export default {
@@ -35,7 +34,7 @@ Sized.args = {
   size: "small",
 };
 
-export const ErroWithMessage: ComponentStory<typeof Select> = () => (
+export const ErrorWithMessage: ComponentStory<typeof Select> = () => (
   <div style={{ width: "150px" }}>
     <Select label="default" data={selectData} error errorMessage="Error!" />
   </div>
@@ -47,16 +46,8 @@ export const Disabled: ComponentStory<typeof Select> = () => (
   </div>
 );
 
-type SelectType = {
-  test: (string | number)[];
-};
-
 export const Multiple: ComponentStory<typeof Select> = () => {
-  const { register, handleSubmit, setValue } = useForm<SelectType>({
-    defaultValues: {
-      test: [],
-    },
-  });
+  const { control, handleSubmit } = useForm();
 
   const onSubmit = (data: any) => {
     console.log(data);
@@ -64,22 +55,21 @@ export const Multiple: ComponentStory<typeof Select> = () => {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} style={{ width: "150px" }}>
-      <Select
-        label="multiple"
-        data={selectData}
-        multiple
-        {...register("test", {
-          onChange: (e: SelectChangeEvent<SelectType["test"]>) => {
-            const {
-              target: { value },
-            } = e;
-            const newValue =
-              typeof value === "string" ? value.split(",") : value;
-            setValue("test", newValue);
-          },
-          value: [],
-        })}
+      <Controller
+        name="multiple"
+        control={control}
+        defaultValue={[]}
+        render={(field) => (
+          <Select
+            {...field}
+            label="multi"
+            data={selectData}
+            multiple
+            defaultValue={[]}
+          />
+        )}
       />
+      <button type="submit">submit</button>
     </form>
   );
 };
