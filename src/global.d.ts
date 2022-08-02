16a7ab649abbc0fa79908231c7545f92c05c0b0f
Insoo-Seo <kocon135@gmail.com>
2022-08-02 12:26:23 +0900

    [chore] dotenv 패키지 설치 및 axiosInstance 생성

---
+++ src/global.d.ts
@@ -0,0 +1,16 @@
+declare module "*.gif" {
+  export default "" as string;
+  export = value;
+}
+declare module "*.png" {
+  export default "" as string;
+  export = value;
+}
+declare module "*.jpg" {
+  export default "" as string;
+  export = value;
+}
+declare module "*.jpeg" {
+  export default "" as string;
+  export = value;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,73 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
-const webpack = require('webpack')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const DotenvPlugin = require("dotenv-webpack");
 
 module.exports = (env, options) => {
-  const prod = options.mode === 'production'
-
+  const prod = options.mode === "production";
   return {
-    mode: prod ? 'production' : 'development',
-    devtool: prod ? 'hidden-source-map' : 'eval',
+    mode: prod ? "production" : "development",
+    devtool: prod ? "hidden-source-map" : "eval",
     resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      extensions: [".js", ".jsx", ".ts", ".tsx"],
       alias: {
-        '@': `${__dirname}/src`
-      }
+        "@": `${__dirname}/src`,
+      },
     },
-    entry: './src/index.tsx',
+    entry: "./src/index.tsx",
     output: {
-      path: path.join(__dirname, '/dist'),
-      filename: '[name].js',
-      publicPath: '/',
-      clean: true
+      path: path.join(__dirname, "/dist"),
+      filename: "[name].js",
+      publicPath: "/",
+      clean: true,
     },
     module: {
       rules: [
         {
           test: /\.(t|j)sx?$/,
           use: [
-            'babel-loader',
+            "babel-loader",
             {
-              loader: 'ts-loader',
+              loader: "ts-loader",
               options: {
-                transpileOnly: true
-              }
-            }
+                transpileOnly: true,
+              },
+            },
           ],
-          exclude: /node_modules/
+          exclude: /node_modules/,
         },
         {
           test: /\.css?$/,
-          use: ['style-loader', 'css-loader']
+          use: ["style-loader", "css-loader"],
         },
         {
           test: /\.(png|jpe?g|svg|gif|webp)$/,
-          loader: 'file-loader',
+          loader: "file-loader",
           options: {
-            name: '[name].[ext]?[hash]'
-          }
-        }
-      ]
+            name: "[name].[ext]?[hash]",
+          },
+        },
+      ],
     },
     devServer: {
       port: 3000,
       hot: true,
-      historyApiFallback: true
+      historyApiFallback: true,
     },
     plugins: [
       new webpack.ProvidePlugin({
-        React: 'react'
+        React: "react",
       }),
       new HtmlWebpackPlugin({
-        template: './public/index.html',
+        template: "./public/index.html",
         minify:
-          process.env.NODE_ENV === 'production'
+          process.env.NODE_ENV === "production"
             ? {
                 collapseWhitespace: true, // 빈칸 제거
-                removeComments: true // 주석 제거
+                removeComments: true, // 주석 제거
               }
-            : false
-      })
-    ]
-  }
-}
+            : false,
+      }),
+      new DotenvPlugin(),
+    ],
+  };
+};
