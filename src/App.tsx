fc7a94fd1fe04e00387ec39ad4c24714cb2a3a35
Insoo-Seo <kocon135@gmail.com>
2022-08-02 23:37:23 +0900

    [fix] kakao redirect페이지 login페이지로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout, { MainPage, NotFoundPage } from "./pages";
-import CallbackKaKaoPage from "./pages/CallbackKaKaoPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/callback/kakao" element={<CallbackKaKaoPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
