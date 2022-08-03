8d1f5217e3689efb70effd310b04ebc657924d3d
JoChaeWoo <jcy6343@naver.com>
2022-08-04 03:42:25 +0900

    [feat] 라이딩의 난이도 입력 추가 라이딩의 난이도를 입력받을 수 있는 컴포넌트를 추가하고 관련 상수를 data.ts 파일에 추가함

---
+++ src/constants/data.ts
@@ -0,0 +1,15 @@
+export const bicycleTypeData = [
+  { id: 0, value: "로드", text: "로드" },
+  { id: 1, value: "MTB", text: "MTB" },
+  { id: 2, value: "픽시", text: "픽시" },
+  { id: 3, value: "하이브리드", text: "하이브리드" },
+  { id: 4, value: "기타", text: "기타" },
+];
+
+export const levelData = [
+  { value: "하", dataLabel: "하" },
+  { value: "중", dataLabel: "중" },
+  { value: "상", dataLabel: "상" },
+];
+
+export const levelDetail = ["응애 난이도", "할만합니다.", "아주아주 힘듭니다."];
