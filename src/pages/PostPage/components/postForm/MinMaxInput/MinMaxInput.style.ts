1cdd37668f5549f32fd008794e89176164216b02
JoChaeWoo <jcy6343@naver.com>
2022-08-04 03:39:41 +0900

    [feat] 최대 최소 인원수 입력 컴포넌트 추가

---
+++ src/pages/PostPage/components/postForm/MinMaxInput/MinMaxInput.style.ts
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import Input from "@/components/Input";
+
+export const Container = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
+
+export const StyledInput = styled(Input)`
+  flex-grow: 1;
+`;
