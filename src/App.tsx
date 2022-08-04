0936332d6109d550fdd98f562e57d49014e9d1bd
Insoo-Seo <kocon135@gmail.com>
2022-08-04 09:44:59 +0900

    [feat] 로그인 유지를 위한 AuthRoute 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout, { MainPage, NotFoundPage } from "./pages";
 import LoginPage from "./pages/LoginPage";
+import AuthRoute from "./routes/AuthRoute";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<MainPage />} />
-          <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<AuthRoute />}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainPage />} />
+            <Route path="/login" element={<LoginPage />} />
+          </Route>
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
