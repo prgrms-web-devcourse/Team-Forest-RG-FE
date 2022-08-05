9b6002ae0bda7a4747a4b5f7060c3cc767a198e0
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:20:20 +0900

    [design] 최대최소 입력 컴포넌트 디자인 수정

--- src/pages/PostPage/components/postForm/MinMaxInput/MinMaxInput.style.ts
+++ src/pages/PostPage/components/postForm/MinMaxInput/MinMaxInput.style.ts
@@ -3,6 +3,12 @@ import Input from "@/components/Input";
 
 export const Container = styled.div`
   display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+export const InputContainer = styled.div`
+  display: flex;
   gap: 0.5rem;
 `;
 
