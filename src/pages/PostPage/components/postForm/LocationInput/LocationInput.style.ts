3c54d643167e10b26577df797b6cd65840d52740
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:18:17 +0900

    [design] LocationInput 디자인 수정 지도 표시 부분의 디자인을 수정함

--- src/pages/PostPage/components/postForm/LocationInput/LocationInput.style.ts
+++ src/pages/PostPage/components/postForm/LocationInput/LocationInput.style.ts
@@ -7,14 +7,24 @@ export const InputContainer = styled.div`
     flex-shrink: 0;
   }
 `;
-
+export const PostSearchWrapper = styled.div`
+  position: absolute;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  z-index: 2;
+  top: 4rem;
+  width: 42rem;
+`;
 export const MapWrapper = styled.div`
   width: 100%;
   height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 1px 1px 3px 1px rgba(0, 0, 0, 0.2);
 `;
 
 export const Container = styled.div`
   display: flex;
+  position: relative;
   flex-direction: column;
   gap: 1rem;
 `;
