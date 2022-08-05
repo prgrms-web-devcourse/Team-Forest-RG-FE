b12605679028c89dc1498fb151c9148e754f58d2
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:15:29 +0900

    [feat] index 파일 추가 각 컴포넌트를 export 하는 index 파일 추가

---
+++ src/pages/PostPage/components/index.ts
@@ -0,0 +1,6 @@
+export { default as AddresInput } from "./AddressInput/AddressInput";
+export { default as ExpandableInput } from "./postForm/ExpandableInput/ExpandableInput";
+export { default as ImagePreview } from "./ImagePreview";
+export { default as MapViewer } from "./postForm/LocationInput/MapViewer";
+export { default as PostCodeSearch } from "./postForm/LocationInput/PostCodeSearch";
+export { default as PostForm } from "./postForm/PostForm";
