849fe8092575b5057bb5184a8b1e8fc289777ebd
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:22:44 +0900

    [design] 난이도 입력 컴포넌트에 header 추가

--- src/pages/PostPage/components/postForm/LevelInput.tsx
+++ src/pages/PostPage/components/postForm/LevelInput.tsx
@@ -4,14 +4,10 @@ import { levelData, levelDetail } from "@/constants/data";
 import Button from "@/components/Button";
 import Radio from "@/components/Radio";
 import Text from "@/components/Text";
-<<<<<<< HEAD
-import { RidingFormValues } from "./PostForm";
-=======
 
 type LevelValue = {
   level: string;
 };
->>>>>>> 53e2993 ([feat] 라이딩의 난이도 입력 추가)
 
 const RadioIconButton = ({
   label,
@@ -27,7 +23,6 @@ const RadioIconButton = ({
   );
 
 function LevelInput({ required = false }: { required?: boolean }) {
-<<<<<<< HEAD
   const { register } = useFormContext<RidingFormValues>();
   const [index, setIndex] = useState<number>(0);
   return (
@@ -35,25 +30,12 @@ function LevelInput({ required = false }: { required?: boolean }) {
       <Text variant="h6">난이도</Text>
       <Radio
         row
-=======
-  const { register } = useFormContext<LevelValue>();
-  const [index, setIndex] = useState<number>(0);
-  return (
-    <div>
-      <Radio
-        row
         label="난이도"
->>>>>>> 53e2993 ([feat] 라이딩의 난이도 입력 추가)
         data={levelData}
         useCustomIcon
         icon={RadioIconButton}
         checkedIcon={RadioIconButton}
-        // TODO Radio 수정 요청
-<<<<<<< HEAD
         {...register("information.level", {
-=======
-        {...register("level", {
->>>>>>> 53e2993 ([feat] 라이딩의 난이도 입력 추가)
           required,
           onChange: (e: ChangeEvent<HTMLInputElement>) => {
             const nowValue = e.target.value;
