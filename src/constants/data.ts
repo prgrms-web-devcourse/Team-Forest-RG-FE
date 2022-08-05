4f272e8867b3d9f40419ceee63304c174264e7ad
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:13:46 +0900

    [feat] 예상시간 입력 컴포넌트 추가

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -3,12 +3,8 @@ export const bicycleTypeData = [
   { id: 1, value: "MTB", text: "MTB" },
   { id: 2, value: "픽시", text: "픽시" },
   { id: 3, value: "하이브리드", text: "하이브리드" },
-<<<<<<< HEAD
   { id: 4, value: "따릉이", text: "따릉이" },
   { id: 5, value: "상관없음", text: "상관없음" },
-=======
-  { id: 4, value: "기타", text: "기타" },
->>>>>>> 53e2993 ([feat] 라이딩의 난이도 입력 추가)
 ];
 
 export const levelData = [
@@ -18,10 +14,8 @@ export const levelData = [
 ];
 
 export const levelDetail = ["응애 난이도", "할만합니다.", "아주아주 힘듭니다."];
-<<<<<<< HEAD
 
 export const estimatedTime = [
-  // "00:30"
   "30분",
   "1시간",
   "1시간 30분",
@@ -35,5 +29,3 @@ export const estimatedTime = [
   "5시간 30분",
   "6시간 이상",
 ];
-=======
->>>>>>> 53e2993 ([feat] 라이딩의 난이도 입력 추가)
