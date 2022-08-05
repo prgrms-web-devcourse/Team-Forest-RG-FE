0e8dae0d90a98450df3ae235d6b5a7e94cdc4728
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:25:02 +0900

    [feat] 포스트 페이지 추가 설정한 라이딩 포스트 개설 페이지 url로 이동 시 표시할 페이지 컴포넌트를 추가함

--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -1,5 +1,19 @@
+import styled from "@emotion/styled";
+import { PostForm } from "./components";
+
+const Container = styled.div`
+  margin: auto;
+  width: fit-content;
+  box-sizing: border-box;
+  padding: 5rem;
+`;
+
 function PostPage() {
-  return <div>PostPage</div>;
+  return (
+    <Container>
+      <PostForm />
+    </Container>
+  );
 }
 
 export default PostPage;
