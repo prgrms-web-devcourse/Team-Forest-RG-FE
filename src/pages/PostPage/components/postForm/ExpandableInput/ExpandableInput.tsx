cfe01ec37096b582e3846c5cdf5e28f3482aef34
JoChaeWoo <jcy6343@naver.com>
2022-08-05 14:21:01 +0900

    [design] ExpandableInput 컴포넌트 디자인 수정

--- src/pages/PostPage/components/postForm/ExpandableInput/ExpandableInput.tsx
+++ src/pages/PostPage/components/postForm/ExpandableInput/ExpandableInput.tsx
@@ -10,13 +10,13 @@ type Section = {
 };
 
 type Sections = {
-  sections: Section[];
+  detail: Section[];
 };
 
 function ExpandableInput() {
   const { control, register } = useFormContext<Sections>();
   const { fields, append, remove } = useFieldArray({
-    name: "sections",
+    name: "detail",
     control,
   });
   return (
@@ -25,22 +25,17 @@ function ExpandableInput() {
         <section className="section" key={field.id}>
           <InputContainer>
             <Input
-              label="소제목"
-              {...register(`sections.${index}.title` as const, {
+              placeholder="소제목"
+              {...register(`detail.${index}.title` as const, {
                 required: true,
                 maxLength: 30,
               })}
             />
-            <input
-              type="file"
-              placeholder="image"
-              multiple
-              {...register(`sections.${index}.image` as const)}
-            />
             <Input
               multiline
               rows={6}
-              {...register(`sections.${index}.content` as const, {
+              placeholder="내용"
+              {...register(`detail.${index}.content` as const, {
                 required: true,
                 maxLength: 500,
               })}
