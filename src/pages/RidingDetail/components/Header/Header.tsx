8b8f842744013b916a8b073a1db7e3806e45345a
syoo970 <syu9710@gmail.com>
2022-08-09 19:32:51 +0900

    [feat] detail 페이지 레이아웃 구성

--- src/pages/RidingDetail/components/Header/Header.tsx
+++ src/pages/RidingDetail/components/Header/Header.tsx
@@ -1,6 +1,6 @@
-import { Grid } from "@mui/material";
 import Avatar from "@/components/Avatar";
-import { Container } from "./Header.style";
+import Text from "@/components/Text";
+import { Container, Content } from "./Header.style";
 
 interface HeaderProps {
   leader: {
@@ -9,21 +9,25 @@ interface HeaderProps {
     profile_image: string;
   };
   thumbnail: string;
+  title: string;
 }
 
-const Header = ({
-  leader: { nickname, profile_image: profileImage },
-  thumbnail,
-}: HeaderProps) => {
+const Header = ({ leader, thumbnail = "", title = "" }: HeaderProps) => {
+  const { nickname, profile_image: profileImage } = leader;
   return (
     <Container>
       <img src={thumbnail} alt="thumbnail" />
-      <Grid container>
-        <Grid item xs={8}>
-          <Avatar src={profileImage} alt="profile" />
-          <span>{nickname}</span>
-        </Grid>
-      </Grid>
+      <Content>
+        <Text variant="h2" noWrap>
+          {title}
+        </Text>
+        <div className="leader-info">
+          <Avatar src={profileImage} alt={nickname} />
+          <Text variant="subtitle1" noWrap>
+            {nickname}
+          </Text>
+        </div>
+      </Content>
     </Container>
   );
 };
