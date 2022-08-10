d98d63218a585f9e5497f8ccbd19b9f69c92cb30
syoo970 <syu9710@gmail.com>
2022-08-10 19:18:48 +0900

    [feat] Tabs 기능 추가

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Avatar as MuiAvatar, AvatarProps } from "@mui/material";
 import StringToColor from "@/utils/StringToColor";
 
@@ -7,36 +8,29 @@ interface CustomAvatarProps extends AvatarProps {
   bgColor?: string;
   name?: string;
   useLetter?: boolean;
+  children?: React.ReactNode;
 }
 
-const Avatar = ({
-  alt,
-  children,
-  src,
-  variant,
-  width,
-  height,
-  bgColor,
-  useLetter,
-  name = "",
-  ...props
-}: CustomAvatarProps) => {
-  return (
-    <MuiAvatar
-      alt={alt}
-      src={src}
-      variant={variant}
-      sx={{
-        width,
-        height,
-        bgColor: useLetter ? bgColor || StringToColor(name) : undefined,
-        display: "inline-flex",
-      }}
-      // eslint-disable-next-line react/no-children-prop
-      children={useLetter ? name[0] : children}
-      {...props}
-    />
-  );
-};
+const Avatar = React.forwardRef<HTMLDivElement, CustomAvatarProps>(
+  (
+    { width, height, bgColor, useLetter, name = "", children, ...props },
+    ref
+  ) => {
+    return (
+      <MuiAvatar
+        sx={{
+          width,
+          height,
+          bgColor: useLetter ? bgColor || StringToColor(name) : undefined,
+          display: "inline-flex",
+        }}
+        // eslint-disable-next-line react/no-children-prop
+        children={useLetter ? name[0] : children}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
 
 export default Avatar;
