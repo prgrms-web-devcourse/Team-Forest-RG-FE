292e6fdbebf94e209dcfb97870e4b6f4d84835fb
JoChaeWoo <jcy6343@naver.com>
2022-08-10 14:09:44 +0900

    [fix] 잘못된 export 제거

--- src/pages/PostPage/components/index.ts
+++ src/pages/PostPage/components/index.ts
@@ -1,4 +1,3 @@
 export { default as ExpandableInput } from "./postForm/ExpandableInput/ExpandableInput";
-export { default as ImagePreview } from "./ImagePreview";
 export { default as MapViewer } from "./postForm/LocationInput/MapViewer";
 export { default as PostForm } from "./postForm/PostForm";
