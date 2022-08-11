94b1153bcba885e20b94e4c9fea3853d226737d6
JoChaeWoo <jcy6343@naver.com>
2022-08-11 10:19:24 +0900

    [refactor] PostForm style 분리

---
+++ src/pages/PostPage/components/postForm/PostForm.style.ts
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+
+export const Form = styled.form`
+  min-width: 60rem;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+`;
+
+export const TwoColumnContainer = styled.div`
+  display: grid;
+  column-gap: 3rem;
+  row-gap: 2rem;
+  grid-template-columns: 1fr 1fr;
+`;
