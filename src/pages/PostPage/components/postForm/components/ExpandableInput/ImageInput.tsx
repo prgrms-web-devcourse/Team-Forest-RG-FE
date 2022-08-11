aa99f875781e9d1ec34e87db47d3f4a5a7d7cd98
JoChaeWoo <jcy6343@naver.com>
2022-08-12 01:21:10 +0900

    [feat] 이미지를 입력하는 컴포넌트 추가. 이미지 파일 입력 시 파일을 Multipart-formdata 방식으로 서버에 저장하는 컴포넌트 추가.

---
+++ src/pages/PostPage/components/postForm/components/ExpandableInput/ImageInput.tsx
@@ -0,0 +1,65 @@
+import {
+  ChangeEventHandler,
+  InputHTMLAttributes,
+  useEffect,
+  useState,
+} from "react";
+import { Image, postImage } from "@/api/image";
+import Button from "@/components/Button";
+import Text from "@/components/Text";
+import ImageViewer from "./ImageViewer/ImageViewer";
+
+interface ImageInputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+  errorMessage?: string;
+  value?: any;
+  onChange?: (...event: any[]) => void;
+  inputRef?: any;
+}
+
+const ImageInput = ({
+  error,
+  errorMessage,
+  value,
+  onChange,
+  inputRef,
+}: ImageInputProps) => {
+  const [imageList, setImageList] = useState<Image[]>([]);
+
+  const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+    if (!e.target.files) return;
+    const images = Array.from(e.target.files);
+    const results = await Promise.all(
+      images.map((image) => {
+        const formData = new FormData();
+        formData.append("image", image);
+        return postImage(formData);
+      })
+    );
+    setImageList((prev) => [...prev, ...results]);
+  };
+
+  useEffect(() => {
+    if (onChange) onChange(imageList.map((item) => item.id));
+  }, [imageList, onChange]);
+
+  return (
+    <div>
+      <Button variant="contained" color="primary" component="label">
+        사진 추가
+        <input
+          hidden
+          accept=".jpg,.png"
+          multiple
+          type="file"
+          onChange={handleChange}
+        />
+      </Button>
+      <ImageViewer images={imageList} />
+      {error && <Text variant="caption">{errorMessage}</Text>}
+      <input hidden value={value} ref={inputRef} />
+    </div>
+  );
+};
+
+export default ImageInput;
