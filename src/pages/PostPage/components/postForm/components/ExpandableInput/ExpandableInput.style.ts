6cdb14e75234887b2ac406b19bbbe6003b205807
JoChaeWoo <jcy6343@naver.com>
2022-08-12 17:04:58 +0900

    [design] ExpandableInput 컴포넌트 디자인 수정

--- src/pages/PostPage/components/postForm/components/ExpandableInput/ExpandableInput.style.ts
+++ src/pages/PostPage/components/postForm/components/ExpandableInput/ExpandableInput.style.ts
@@ -6,3 +6,24 @@ export const InputContainer = styled.div`
   flex-direction: column;
   margin-bottom: 1rem;
 `;
+
+export const Section = styled.section`
+  margin: 1rem 0;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  & > button {
+    align-self: flex-end;
+  }
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+  & button {
+    flex-shrink: 0;
+    min-height: 3.5rem;
+  }
+`;
