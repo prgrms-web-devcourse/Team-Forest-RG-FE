90f5dd4bbfbfbbf280fb5833b5babfff89693171
JoChaeWoo <jcy6343@naver.com>
2022-08-13 00:23:32 +0900

    [feat] RouteInput을 제어형 컴포넌트로 변경

--- src/pages/PostPage/components/postForm/components/RouteInput/RouteInput.tsx
+++ src/pages/PostPage/components/postForm/components/RouteInput/RouteInput.tsx
@@ -1,84 +1,77 @@
-import { useFormContext } from "react-hook-form";
-import { createRef, useCallback, useEffect, useState } from "react";
+import { forwardRef, useCallback, useState } from "react";
 import { Breadcrumbs, Icon } from "@mui/material";
-import { RidingFormValues } from "../../PostForm";
 import { Container } from "./RouteInput.style";
 import Input from "@/components/Input";
 import Chip from "@/components/Chip";
 
-function RouteInput() {
-  const { register, setValue, setError } = useFormContext<RidingFormValues>();
-  const [route, setRoute] = useState<string[]>([]);
-  const inputRef = createRef<HTMLInputElement>();
-
-  const handleKeydown = useCallback(
-    (e: KeyboardEvent) => {
-      if (e.key !== "Enter") return;
-      if (!(e.target instanceof HTMLInputElement)) return;
-      e.preventDefault();
-      const inputValue = e.target.value;
-      if (inputValue.length < 1) {
-        setError("information.routes", {
-          type: "custom",
-          message: "1글자 이상 입력해주세요",
-        });
-      } else if (route.length >= 5) {
-        setError("information.routes", {
-          type: "custom",
-          message: "5개까지 입력 가능합니다.",
-        });
-      } else {
-        setRoute((prev) => [...prev, inputValue]);
-        e.target.value = "";
-      }
-    },
-    [route, setError]
-  );
-
-  useEffect(() => {
-    const chipInput = inputRef.current;
-    chipInput?.addEventListener("keypress", handleKeydown);
-    return () => {
-      chipInput?.removeEventListener("keypress", handleKeydown);
-    };
-  }, [handleKeydown, inputRef]);
-
-  useEffect(() => {
-    setValue("information.routes", route, {
-      shouldValidate: route.length !== 0,
-    });
-  }, [route, setValue]);
-
-  return (
-    <>
-      <Container>
-        <Input fullWidth ref={inputRef} />
-        <Breadcrumbs
-          separator={<Icon fontSize="small">arrow_forward_ios_icon</Icon>}
-        >
-          {route.map((item, index) => (
-            <Chip
-              label={item}
-              key={item}
-              variant="outlined"
-              onDelete={() => {
-                setRoute((prev) =>
-                  prev.filter((_, prevIndex) => index !== prevIndex)
-                );
-              }}
-            />
-          ))}
-        </Breadcrumbs>
-      </Container>
-      <input
-        hidden
-        {...register("information.routes", {
-          required: true,
-          validate: { maxLength: (value) => value.length <= 5 },
-        })}
-      />
-    </>
-  );
+interface RouteInputProps {
+  onChange: (...event: any) => void;
+  onBlur?: (...event: any) => void;
+  value?: string[];
 }
+
+const RouteInput = forwardRef<HTMLInputElement, RouteInputProps>(
+  ({ onChange, onBlur, value = [] }, ref) => {
+    const [error, setError] = useState<{
+      error: boolean;
+      message: string;
+    }>({ error: false, message: "" });
+
+    const handleKeydown = useCallback(
+      (e: KeyboardEvent) => {
+        setError({ error: false, message: "" });
+        if (e.key !== "Enter") return;
+        if (!(e.target instanceof HTMLInputElement)) return;
+        e.preventDefault();
+        const inputValue = e.target.value;
+
+        if (inputValue.length < 1) {
+          setError({ error: true, message: "1글자 이상 입력해주세요" });
+        } else if (inputValue.length >= 15) {
+          setError({ error: true, message: "최대 15자 입력 가능합니다." });
+        } else if (value.length >= 5) {
+          setError({ error: true, message: "5개까지 추가할 수 있습니다." });
+        } else {
+          onChange([...value, inputValue]);
+          e.target.value = "";
+        }
+      },
+      [onChange, value]
+    );
+
+    return (
+      <>
+        <Container>
+          <Input
+            fullWidth
+            onKeyDown={handleKeydown}
+            onBlur={() => {
+              if (onBlur) onBlur();
+              setError({ error: false, message: "" });
+            }}
+            placeholder="행선지 추가 (최대 15자)"
+            error={error.error}
+            errorMessage={error.message}
+          />
+          <Breadcrumbs
+            separator={<Icon fontSize="small">arrow_forward_ios_icon</Icon>}
+          >
+            {value.map((item, index) => (
+              <Chip
+                label={item}
+                key={item}
+                variant="outlined"
+                onDelete={() => {
+                  onChange(value.filter((_, prevIndex) => index !== prevIndex));
+                }}
+              />
+            ))}
+          </Breadcrumbs>
+        </Container>
+        <input hidden readOnly ref={ref} />
+      </>
+    );
+  }
+);
 
 export default RouteInput;
