cf53abdb11c39da823ad5244fe82c9912d4bdec0
JoChaeWoo <jcy6343@naver.com>
2022-08-13 00:21:07 +0900

    [feat] RegionInput 컴포넌트 기능 추가 value를 입력했을 때, 각 select 메뉴가 ui로 표시되는 기능 추가

--- src/pages/PostPage/components/postForm/components/RegionInput/RegionInput.tsx
+++ src/pages/PostPage/components/postForm/components/RegionInput/RegionInput.tsx
@@ -1,48 +1,53 @@
-import { useFormContext } from "react-hook-form";
-import { useMemo, useState } from "react";
-import { SelectChangeEvent } from "@mui/material";
+import { forwardRef, useMemo } from "react";
 import Select from "@/components/Select";
 import { regionCode } from "@/constants/region";
 import { Container } from "./RegionInput.style";
-import { RidingFormValues } from "../../PostForm";
 
-function RegionInput() {
-  const { register } = useFormContext<RidingFormValues>();
-  const [cityCode, setCityCode] = useState<number>(0);
-  const detailData = useMemo(() => {
-    const result = regionCode
-      .find(({ code }) => code === cityCode)
-      ?.detail.map(({ code, name }) => ({
-        key: code,
-        value: code,
-        text: name,
-      }));
-    return result || [{ key: 0, value: 0, text: "" }];
-  }, [cityCode]);
-  return (
-    <Container>
-      <Select
-        label="시/도"
-        data={regionCode.map(({ code, name }) => ({
+interface RegionInputProps {
+  onChange?: (...event: any[]) => void;
+  value?: number;
+}
+
+const RegionInput = forwardRef<HTMLInputElement, RegionInputProps>(
+  ({ onChange, value }, ref) => {
+    const cityCode = value && Math.floor(value / 1000);
+
+    const detailData = useMemo(() => {
+      const result = regionCode
+        .find(({ code }) => code === cityCode)
+        ?.detail.map(({ code, name }) => ({
           key: code,
           value: code,
           text: name,
-        }))}
-        required
-        onChange={(e: SelectChangeEvent<unknown>) =>
-          setCityCode(Number(e.target.value))
-        }
-      />
-      <Select
-        label="시/군/구"
-        data={detailData}
-        {...register("information.regionCode", {
-          required: true,
-          disabled: cityCode === 0,
-        })}
-      />
-    </Container>
-  );
-}
+        }));
+      return result || [{ key: 0, value: 0, text: "" }];
+    }, [cityCode]);
+    return (
+      <Container>
+        <Select
+          label="시/도"
+          data={regionCode.map(({ code, name }) => ({
+            key: code,
+            value: code,
+            text: name,
+          }))}
+          value={value && Math.floor(value / 1000)}
+          onChange={(e) => {
+            if (onChange) onChange(Number(e.target.value) * 1000);
+          }}
+          ref={ref}
+        />
+
+        <Select
+          label="시/군/구"
+          data={detailData}
+          value={value}
+          onChange={onChange}
+          ref={ref}
+        />
+      </Container>
+    );
+  }
+);
 
 export default RegionInput;
