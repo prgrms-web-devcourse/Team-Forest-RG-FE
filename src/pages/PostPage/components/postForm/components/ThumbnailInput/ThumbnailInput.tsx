61797cf7e4591727cf3b871de69d4115c607a7a8
JoChaeWoo <jcy6343@naver.com>
2022-08-13 00:22:51 +0900

    [refactor] ThumbnailInput 컴포넌트 리팩토링 forwardRef를 사용해서 별도 props 없이 ref에 접근 가능하도록 리팩토링

--- src/pages/PostPage/components/postForm/components/ThumbnailInput/ThumbnailInput.tsx
+++ src/pages/PostPage/components/postForm/components/ThumbnailInput/ThumbnailInput.tsx
@@ -1,6 +1,5 @@
-import { ChangeEventHandler, useState } from "react";
+import { ChangeEventHandler, forwardRef, useState } from "react";
 import { postImage } from "@/api/image";
-import ridingThumbnailExample from "@/assets/riding_thumbnail_example.png";
 import Button from "@/components/Button";
 import Text from "@/components/Text";
 import {
@@ -11,36 +10,39 @@ import {
 
 interface ThumbnailInptProps {
   onChange?: (...event: any[]) => void;
-  inputRef?: any;
+  defaultUrl: string;
 }
-const ThumbnailInput = ({ onChange, inputRef }: ThumbnailInptProps) => {
-  const [imageUrl, setImageUrl] = useState<string>(ridingThumbnailExample);
-  const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
-    if (!e.target.files) return;
-    const formData = new FormData();
-    formData.append("image", e.target.files[0]);
-    const image = await postImage(formData);
-    setImageUrl(image.url);
-    if (onChange) onChange(image.id);
-  };
-  return (
-    <Container>
-      <Text variant="h6">대표 이미지</Text>
-      <ThumbnailContainer>
-        <ThumbnailImage src={imageUrl} alt="thumbnail" />
-        <Button customColor="#999" component="label">
-          등록하기
-          <input
-            hidden
-            accept=".jpg,.png"
-            type="file"
-            ref={inputRef}
-            onChange={handleChange}
-          />
-        </Button>
-      </ThumbnailContainer>
-    </Container>
-  );
-};
+const ThumbnailInput = forwardRef<HTMLInputElement, ThumbnailInptProps>(
+  ({ onChange, defaultUrl }, ref) => {
+    const [imageUrl, setImageUrl] = useState<string>(defaultUrl);
+
+    const handleChange: ChangeEventHandler<HTMLInputElement> = async (e) => {
+      if (!e.target.files) return;
+      const formData = new FormData();
+      formData.append("image", e.target.files[0]);
+      const image = await postImage(formData);
+      setImageUrl(image.url);
+      if (onChange) onChange(image.id);
+    };
+    return (
+      <Container>
+        <Text variant="h6">대표 이미지</Text>
+        <ThumbnailContainer>
+          <ThumbnailImage src={imageUrl} alt="thumbnail" />
+          <Button customColor="#999" component="label">
+            등록하기
+            <input
+              hidden
+              accept=".jpg,.png"
+              type="file"
+              ref={ref}
+              onChange={handleChange}
+            />
+          </Button>
+        </ThumbnailContainer>
+      </Container>
+    );
+  }
+);
 
 export default ThumbnailInput;
