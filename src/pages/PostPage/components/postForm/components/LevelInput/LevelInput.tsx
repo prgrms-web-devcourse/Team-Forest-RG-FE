6a1a588056bcf210b8d7b02163ff9f690f5737ed
JoChaeWoo <jcy6343@naver.com>
2022-08-12 17:03:32 +0900

    [design, fix] 디자인 수정, 불필요한 props 제거

--- src/pages/PostPage/components/postForm/components/LevelInput/LevelInput.tsx
+++ src/pages/PostPage/components/postForm/components/LevelInput/LevelInput.tsx
@@ -4,7 +4,8 @@ import { levelData, levelDetail } from "@/constants/data";
 import Button from "@/components/Button";
 import Radio from "@/components/Radio";
 import Text from "@/components/Text";
-import { RidingFormValues } from "../PostForm";
+import { RidingFormValues } from "../../PostForm";
+import { Container } from "./LevelInput.style";
 
 const RadioIconButton = ({
   label,
@@ -19,12 +20,11 @@ const RadioIconButton = ({
     <Button customColor="#999">{label}</Button>
   );
 
-function LevelInput({ required = false }: { required?: boolean }) {
+function LevelInput() {
   const { register } = useFormContext<RidingFormValues>();
   const [index, setIndex] = useState<number>(0);
   return (
-    <div>
-      <Text variant="h6">난이도</Text>
+    <Container>
       <Radio
         row
         data={levelData}
@@ -32,7 +32,6 @@ function LevelInput({ required = false }: { required?: boolean }) {
         icon={RadioIconButton}
         checkedIcon={RadioIconButton}
         {...register("information.level", {
-          required,
           onChange: (e: ChangeEvent<HTMLInputElement>) => {
             const nowValue = e.target.value;
             if (nowValue === "하") setIndex(0);
@@ -42,7 +41,7 @@ function LevelInput({ required = false }: { required?: boolean }) {
         })}
       />
       <Text>{levelDetail[index]}</Text>
-    </div>
+    </Container>
   );
 }
 
