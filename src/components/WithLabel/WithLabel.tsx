248ef46fae89a7a00dee4ea7f3e7beb61b66afd2
JoChaeWoo <jcy6343@naver.com>
2022-08-12 17:01:23 +0900

    [feat] WithLabel 컴포넌트 기능 추가 캡션, 필수 여부 표시 기능 추가.

--- src/components/WithLabel/WithLabel.tsx
+++ src/components/WithLabel/WithLabel.tsx
@@ -1,24 +1,39 @@
 import { TypographyProps } from "@mui/material";
 import Text from "@/components/Text";
 import { Container } from "./WithLabel.style";
+import { CustomTypographyProps } from "../Text/Text";
 
 interface WithLabelProps {
   variant?: TypographyProps["variant"];
   label: string;
+  caption?: string;
   children: any;
   wd?: number;
+  isRequired?: boolean;
+  labelProps?: Partial<CustomTypographyProps>;
 }
 
 const WithLabel = ({
   variant = "h5",
   label,
   children,
+  caption,
   wd,
+  isRequired,
+  labelProps,
   ...props
 }: WithLabelProps) => {
   return (
     <Container width={wd} {...props}>
-      <Text variant={variant}>{label}</Text>
+      <Text variant={variant} {...labelProps}>
+        {label}
+        {isRequired && (
+          <Text variant="caption" textStyle={{ color: "red" }}>
+            &nbsp;*
+          </Text>
+        )}
+        {caption && <Text variant="caption">&nbsp;{caption}</Text>}
+      </Text>
       {children}
     </Container>
   );
