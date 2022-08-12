87e216d9b07d642950de4151dd21ac0cac4a24bc
JoChaeWoo <jcy6343@naver.com>
2022-08-12 17:04:58 +0900

    [design] ExpandableInput 컴포넌트 디자인 수정

--- src/pages/PostPage/components/postForm/components/ExpandableInput/ExpandableInput.tsx
+++ src/pages/PostPage/components/postForm/components/ExpandableInput/ExpandableInput.tsx
@@ -1,6 +1,11 @@
 import { Controller, useFieldArray, useFormContext } from "react-hook-form";
 import Input from "@/components/Input";
-import { InputContainer } from "./ExpandableInput.style";
+import {
+  Container,
+  InputContainer,
+  Section,
+  TitleContainer,
+} from "./ExpandableInput.style";
 import Button from "@/components/Button";
 import ImageInput from "./ImageInput";
 import { RidingFormValues } from "../../PostForm";
@@ -12,44 +17,63 @@ function ExpandableInput() {
     control,
   });
   return (
-    <>
+    <Container>
       {fields.map((field, index) => (
-        <section className="section" key={field.id}>
+        <Section className="section" key={field.id}>
           <InputContainer>
-            <Input
-              placeholder="소제목"
-              {...register(`detail.${index}.title` as const, {
-                required: true,
-                maxLength: 30,
-              })}
-            />
+            <TitleContainer>
+              <Input
+                placeholder="소제목 (최대 30자)"
+                fullWidth
+                {...register(`detail.${index}.title` as const, {
+                  required: true,
+                  maxLength: 30,
+                })}
+              />
+              <Button color="error" onClick={() => remove(index)}>
+                섹션 삭제
+              </Button>
+            </TitleContainer>
             <Controller
               control={control}
               render={({ field: { onChange, value, ref } }) => (
-                <ImageInput onChange={onChange} value={value} inputRef={ref} />
+                <ImageInput
+                  onChange={onChange}
+                  value={value}
+                  inputRef={ref}
+                  imageLimit={2}
+                />
               )}
+              rules={{
+                validate: {
+                  maxImageNum: (value) => {
+                    return (
+                      value.length <= 2 || "최대 2개까지 추가할 수 있습니다."
+                    );
+                  },
+                },
+              }}
               name={`detail.${index}.images`}
             />
             <Input
               multiline
               rows={6}
-              placeholder="내용"
+              placeholder="내용 (최대 500자)"
               {...register(`detail.${index}.content` as const, {
                 required: true,
                 maxLength: 500,
               })}
             />
           </InputContainer>
-          <Button onClick={() => remove(index)}>문단 삭제</Button>
-        </section>
+        </Section>
       ))}
       <Button
         type="button"
         onClick={() => append({ title: "", images: [], content: "" })}
       >
-        문단 추가
+        섹션 추가
       </Button>
-    </>
+    </Container>
   );
 }
 
