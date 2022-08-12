d7c741f42894309a7b91b7f0a8f46d2daef3136b
Insoo-Seo <kocon135@gmail.com>
2022-08-12 22:19:55 +0900

    [feat] ProfileModify  구현

--- src/pages/MyPage/components/Profile/ProfileModify.tsx
+++ src/pages/MyPage/components/Profile/ProfileModify.tsx
@@ -1,10 +1,27 @@
 import { useRecoilValue } from "recoil";
 import { userState } from "@/recoil/state/authState";
+import useUserInfo from "../../hooks/useUserInfo";
+import ProfileModifyForm from "@/pages/MyPage/components/Profile/ProfileModifyForm";
 
 function ProfileModify() {
   const myUserId = useRecoilValue(userState);
+  const [userInfo, loading] = useUserInfo(myUserId);
+  console.log(userInfo);
+  const dummyUserInfo = {
+    nickname: "out",
+    ridingStartYear: "2020",
+    favoriteRegionCode: 11,
+    level: "상",
+    bicycles: ["따릉이"],
+  };
 
-  return <div>프로필 수정 탭 id: {myUserId}</div>;
+  if (loading) return <div>Loading</div>;
+  return (
+    <div>
+      프로필 수정 탭 id: {myUserId}
+      <ProfileModifyForm userInfo={dummyUserInfo} />
+    </div>
+  );
 }
 
 export default ProfileModify;
