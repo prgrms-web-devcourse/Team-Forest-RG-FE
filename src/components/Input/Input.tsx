cc07aed9d401294c8f55fc0325a3b01ea6cb8625
JoChaeWoo <jcy6343@naver.com>
2022-08-13 00:14:34 +0900

    [feat] 컴포넌트 구현에 필요한 props 추가

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -22,6 +22,9 @@ interface InputProps {
   placeholder?: string;
   defaultValue?: string | number | Date;
   type?: string;
+  onChange?: (...event: any) => void;
+  onKeyDown?: (...event: any) => void;
+  onBlur?: (...event: any) => void;
   inputRef?: RefObject<HTMLInputElement>;
   InputLabelProps?: TextFieldProps["InputLabelProps"];
 }
@@ -45,6 +48,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       multiline,
       rows,
       rowsMax,
+      onChange,
+      onKeyDown,
+      onBlur,
       placeholder,
       defaultValue,
       InputLabelProps,
@@ -83,6 +89,9 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         defaultValue={defaultValue}
         InputLabelProps={InputLabelProps}
         type={type}
+        onChange={onChange}
+        onBlur={onBlur}
+        onKeyDown={onKeyDown}
         {...props}
       />
     );
