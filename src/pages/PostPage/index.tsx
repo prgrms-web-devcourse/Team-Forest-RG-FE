b5316828a712f2e3684c4a00b5de9c3c5245c9c5
JoChaeWoo <jcy6343@naver.com>
2022-08-13 15:13:07 +0900

    [feat] api 연동 form의 데이터를 api로 서버에 전달하고 페이지를 이동하는 기능 추가

--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
+import { SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 import { PostForm } from "./components";
 import { RidingFormValues } from "./components/postForm/PostForm";
+import { postPosts } from "@/api/posts";
 
 const Container = styled.div`
   margin: auto;
@@ -10,8 +13,10 @@ const Container = styled.div`
 `;
 
 function PostPage() {
-  const handleSubmit = (data: RidingFormValues) => {
-    console.log(data);
+  const navigate = useNavigate();
+  const handleSubmit: SubmitHandler<RidingFormValues> = async (data) => {
+    const postId = await postPosts(data);
+    navigate(`/post/${postId}`, { replace: true });
   };
   return (
     <Container>
