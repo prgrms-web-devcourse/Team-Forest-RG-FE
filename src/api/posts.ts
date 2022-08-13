b7e2528ab080dbd31436021cc7147cda84290a7b
JoChaeWoo <jcy6343@naver.com>
2022-08-14 05:44:37 +0900

    [rename] api 함수명 변경

--- src/api/posts.ts
+++ src/api/posts.ts
@@ -2,7 +2,7 @@ import * as T from "response";
 import { RidingFormValues } from "@/pages/PostPage/components/postForm/PostForm";
 import axiosInstance from "./axiosInstance";
 
-export const getPosts = async (postId: number): Promise<T.PostDetail> => {
+export const getPost = async (postId: number): Promise<T.PostDetail> => {
   const response = await axiosInstance({
     method: "GET",
     url: `/api/v1/ridingposts/${postId}`,
@@ -10,7 +10,7 @@ export const getPosts = async (postId: number): Promise<T.PostDetail> => {
   return response.data;
 };
 
-export const postPosts = async (
+export const postPost = async (
   postData: RidingFormValues
 ): Promise<T.PostDetail> => {
   const response = await axiosInstance({
