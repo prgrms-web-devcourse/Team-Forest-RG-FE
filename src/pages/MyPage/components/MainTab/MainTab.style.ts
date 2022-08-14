262ef2faa64ce5e0380e5b2302feb3763f4a9638
JoChaeWoo <jcy6343@naver.com>
2022-08-15 05:58:16 +0900

    [feat] 마이페이지 메인 페이지 구현

---
+++ src/pages/MyPage/components/MainTab/MainTab.style.ts
@@ -0,0 +1,50 @@
+import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
+
+export const StyledLink = styled(Link)`
+  display: flex;
+  gap: 0.25rem;
+  color: black;
+  align-items: center;
+  text-decoration: none;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 1rem 2rem;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProfileContainer = styled.div`
+  box-sizing: border-box;
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 2rem;
+  border-radius: 0.5rem;
+`;
+
+export const MenuContainer = styled.div`
+  box-sizing: border-box;
+  padding: 2rem;
+  display: flex;
+  gap: 2.5rem;
+  justify-content: center;
+`;
+
+export const RidingContainer = styled.div`
+  box-sizing: border-box;
+  padding: 2rem 0;
+  display: flex;
+  gap: 1rem;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LinkButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 3rem 0;
+`;
---
+++ src/pages/MyPage/components/MainTab/MainTab.tsx
@@ -0,0 +1,78 @@
+import { useRecoilValue } from "recoil";
+import { Avatar, Icon } from "@mui/material";
+import { userState } from "@/recoil/state/authState";
+import useUserInfo from "../../hooks/useUserInfo";
+import Spinner from "@/components/Spinner";
+import Text from "@/components/Text";
+import {
+  Container,
+  LinkButtonWrapper,
+  MenuContainer,
+  ProfileContainer,
+  RidingContainer,
+  StyledLink,
+} from "./MainTab.style";
+import Divider from "@/components/Divider";
+import Button from "@/components/Button";
+
+const MainTab = () => {
+  const myUserId = useRecoilValue(userState);
+  const [userInfo, loading] = useUserInfo(myUserId);
+  console.log(userInfo);
+  return !userInfo || loading ? (
+    <Spinner />
+  ) : (
+    <Container>
+      <Text variant="h5" textStyle={{ fontWeight: 600 }}>
+        마이 페이지
+      </Text>
+      <ProfileContainer>
+        <Avatar src={userInfo.ridingProfile.profileImage} />
+        <Text variant="subtitle1">{userInfo.ridingProfile.introduction}</Text>
+      </ProfileContainer>
+      <Divider />
+      <MenuContainer>
+        <StyledLink to="/mypage/riding">
+          <Icon fontSize="large">pedal_bike_icon</Icon>
+          <Text variant="subtitle1">라이딩 내역</Text>
+        </StyledLink>
+        <Divider direction="vertical" />
+        <StyledLink to="/mypage/riding">
+          <Icon fontSize="large">fact_check_icon</Icon>
+          <Text variant="subtitle1">평가</Text>
+        </StyledLink>
+        <Divider direction="vertical" />
+        <StyledLink to="/mypage/riding">
+          <Icon fontSize="large">settings_icon</Icon>
+          <Text variant="subtitle1">프로필 수정</Text>
+        </StyledLink>
+      </MenuContainer>
+      <Divider />
+      <RidingContainer>
+        <Text variant="h6" textStyle={{ fontWeight: 600 }}>
+          평가 가능한 라이딩
+        </Text>
+        <Text variant="body1">
+          {userInfo.ridings.canEvaluated.length !== 0
+            ? `평가 가능한 라이딩이 ${userInfo.ridings.canEvaluated.length}개 있어요`
+            : `평가 가능한 라이딩이 없어요. 라이딩을 신청하고 같이 간 사용자를 추천해 보세요!`}
+        </Text>
+        {userInfo.ridings.canEvaluated.length !== 0 ? (
+          <LinkButtonWrapper>
+            <StyledLink to="/mypage/evaluate">
+              <Button variant="outlined">라이딩 평가하러 가기</Button>
+            </StyledLink>
+          </LinkButtonWrapper>
+        ) : (
+          <LinkButtonWrapper>
+            <StyledLink to="/posts">
+              <Button variant="outlined">라이딩 찾아보기</Button>
+            </StyledLink>
+          </LinkButtonWrapper>
+        )}
+      </RidingContainer>
+    </Container>
+  );
+};
+
+export default MainTab;
