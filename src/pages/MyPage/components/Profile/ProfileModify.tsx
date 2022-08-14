0dadfbc7aa55f7b081bff8a7638e7f94b49bd31e
JoChaeWoo <jcy6343@naver.com>
2022-08-14 19:15:42 +0900

    [feat] 수정 완료 이후 유저 정보 갱신 기능 추가

--- src/pages/MyPage/components/Profile/ProfileModify.tsx
+++ src/pages/MyPage/components/Profile/ProfileModify.tsx
@@ -1,17 +1,23 @@
 import { useRecoilValue } from "recoil";
 import { SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
 import { userState } from "@/recoil/state/authState";
 import useUserInfo from "../../hooks/useUserInfo";
 import ProfileModifyForm from "@/pages/MyPage/components/Profile/ProfileModifyForm";
 import user, { RegisterData } from "@/api/user";
 
 function ProfileModify() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const myUserId = useRecoilValue(userState);
   const [userInfo, loading] = useUserInfo(myUserId);
 
   const onSubmit: SubmitHandler<RegisterData> = async (data) => {
     if (!myUserId) return;
     await user.setUserInfo(myUserId, data);
+    queryClient.invalidateQueries(["user-fetch"]);
+    navigate("/mypage");
   };
 
   if (loading) return <div>Loading</div>;
