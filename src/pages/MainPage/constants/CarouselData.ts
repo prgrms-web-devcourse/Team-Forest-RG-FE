94d72d55a86c71f1234b36f5bbcc6f06c0e75321
syoo970 <syu9710@gmail.com>
2022-08-15 16:26:56 +0900

    [style] banner 변경

--- src/pages/MainPage/constants/CarouselData.ts
+++ src/pages/MainPage/constants/CarouselData.ts
@@ -4,21 +4,21 @@ const mainCarouselData = [
   {
     id: 1,
     component: React.createElement("img", {
-      src: "https://pixel.nymag.com/imgs/daily/vulture/2017/06/14/14-tom-cruise.jpg",
+      src: "https://mygbs.s3.ap-northeast-2.amazonaws.com/rgbanner/banner1.png",
       alt: "carousel",
     }),
   },
   {
     id: 2,
     component: React.createElement("img", {
-      src: "https://cdn.pixabay.com/photo/2022/04/08/21/04/buildings-7120297_960_720.jpg",
+      src: "https://mygbs.s3.ap-northeast-2.amazonaws.com/rgbanner/banner2.png",
       alt: "carousel",
     }),
   },
   {
     id: 3,
     component: React.createElement("img", {
-      src: "https://unsplash.it/1200/800?image=10",
+      src: "https://mygbs.s3.ap-northeast-2.amazonaws.com/rgbanner/banner3.png",
       alt: "carousel",
     }),
   },
