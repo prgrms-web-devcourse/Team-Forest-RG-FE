07bf706ff61f55d27ffbe9e2e7a0583d99b8db86
JoChaeWoo <jcy6343@naver.com>
2022-08-16 01:23:23 +0900

    [design] RegisterPage 디자인 변경

---
+++ src/pages/RegisterPage/RegisterPage.style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  box-sizing: border-box;
+  padding-top: 2rem;
+  margin: 0 auto;
+  max-width: 50rem;
+`;
--- src/pages/RegisterPage/index.tsx
+++ src/pages/RegisterPage/index.tsx
@@ -1,10 +1,16 @@
+import Text from "@/components/Text";
 import RegisterForm from "./components/RegisterForm";
+import { Container } from "./RegisterPage.style";
 
 function RegisterPage() {
   return (
-    <div>
+    <Container>
+      <Text variant="h5" fontWeight={600}>
+        라이딩 정보 입력
+      </Text>
+      <Text variant="body1">라이딩 매칭에 필요한 정보를 입력해주세요.</Text>
       <RegisterForm />
-    </div>
+    </Container>
   );
 }
 
